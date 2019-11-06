1ae0774c5c2f1f12df69cc73d4c3e7f5b2f8b5cb
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-06 14:32:23 -0800

    add root/login/signup routes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_many :created_workouts, foreign_key: "user_id", class_name: "Workout"
   has_many :plans
-  # The :source option specifies the source association name for a has_many :through association
+  # The :source option specifies the source association name for a has_many :through association, this one specifically is dependent on my Plan Class
   has_many :completed_workouts, through: :plans,  source: :workouts
 
 end
