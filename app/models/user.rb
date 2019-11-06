6ea8495f21acb6f4e536070fea85311e7ae17659
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-06 14:24:23 -0800

    fix user associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-  has_many: :created_workouts, foreign_key: "user_id", class_name: "Workout"
+  has_many :created_workouts, foreign_key: "user_id", class_name: "Workout"
   has_many :plans
   # The :source option specifies the source association name for a has_many :through association
   has_many :completed_workouts, through: :plans,  source: :workouts
