8c1ed7b7dd891eb9c4d13cf986a1480b8b867396
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-06 17:34:28 -0800

    add show/create controller actions

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -4,6 +4,26 @@ class WorkoutsController < ApplicationController
   end
 
   def index 
-    
+  end
+
+  def show
+    @workout = Workout.find_by(id: params[:id])
+  end
+
+  def create
+    # "workout"=>{"title"=>"Workout A", "category"=>"push"}
+    @workout = current_user.created_workouts.build(workout_params)
+    if @workout.save
+      redirect_to workout_path(@workout)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def workout_params
+    params.require(:workout).permit(:title, :category)
   end
 end
