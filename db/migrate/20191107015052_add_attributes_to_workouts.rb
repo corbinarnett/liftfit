e0f2aa3d4e649a3af2e60811e1d8fb474768fc00
corbinarnett <corbin.c.arnett@gmail.com>
2019-11-06 18:17:34 -0800

    delete attributes from work out, move to exercise

--- db/migrate/20191107015052_add_attributes_to_workouts.rb
+++
@@ -1,7 +0,0 @@
-class AddAttributesToWorkouts < ActiveRecord::Migration[6.0]
-  def change
-    add_column :workouts, :sets_per_exercise, :integer
-    add_column :workouts, :rep_range, :integer
-    add_column :workouts, :rest_time_between_sets, :integer
-  end
-end
